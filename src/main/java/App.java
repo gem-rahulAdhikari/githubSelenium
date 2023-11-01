5daa9abeb85540584459a263f073a807913d9c18
gem-rahulAdhikari <103276055+gem-rahulAdhikari@users.noreply.github.com>
2023-11-01 10:47:15 +0530

    Update YAML and Java files

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,183 +1,66 @@
-Report_WedNov110:25:552023"selenium-reporting";
+import com.aventstack.extentreports.Status;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
 
-    public static String readClassFileAsString(String filePath) throws IOException {
-        //Reading user-updated code
-        StringBuilder content = new StringBuilder();
+import java.lang.*;
+import java.util.*;
+import java.awt.*;
+import java.math.*;
+import java.time.*;
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                content.append(line).append('\n');
-            }
-        }
+public class App extends driverConfig{
+    static String reportName="Report_WedNov110:25:552023";
 
-        return content.toString();
-    }
+@Test
+public void demo(){
+driver.get("https://www.google.com/");
+     WebElement searchInput = driver.findElement(By.xpath("//textarea"));
+    searchInput.sendKeys("selenium");
+    searchInput.sendKeys(Keys.RETURN);
+     WebElement title = driver.findElement(By.xpath("(//h3[text()='Selenium'])[1]")); 
+     String fetchedTitle=title.getText();
+    System.out.println(fetchedTitle+" start2");
+     if("Selenium".equals(fetchedTitle))
+     {
+         extentTest.log(Status.PASS,"text matched successfully.",captureScreenshot());
+     }
+    else
+     {
+         extentTest.log(Status.FAIL,"Failed to match text.",captureScreenshot());
+     }
 
-    @BeforeSuite
-    public void reporter() {
-        //Extent report initialization
-        ExtentSparkReporter htmlReporter = new ExtentSparkReporter("test-output/" + App.reportName + ".html");
-        extentReports = new ExtentReports();
-        extentReports.attachReporter(htmlReporter);
-        extentTest = extentReports.createTest(getClass().getSimpleName());
-        extentReports.setSystemInfo("OS Info", System.getProperty("os.name"));
-        extentReports.setSystemInfo("Java Version", System.getProperty("java.specification.version"));
-    }
+     extentTest.log(Status.PASS,driver.getCurrentUrl(),captureScreenshot());
 
-    @BeforeMethod
-    public void setWebDriver() {
-        //Chromedriver setup
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
-        options.addArguments("start-maximized");
-        options.addArguments("disable-infobars");
-        options.addArguments("--disable-extensions");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--remote-allow-origins=*");
-        WebDriverListener listener = new WebdriverEventListener();
-        wDriver.set(new ChromeDriver(options));
-        WebDriver decorated = new EventFiringDecorator(listener).decorate(wDriver.get());
-        wDriver.set(decorated);
-        driver = wDriver.get();
-        System.out.println(driver);
-    }
-
-    @AfterMethod
-    public void tearDown() {
-        //terminating execution
-        driver.quit();
-        extentReports.flush();
-    }
-
-    @AfterSuite
-    public void reportMover() throws IOException {
-        //Uploading report to gcloud bucket storage
-        System.out.println("Execution complete, report manipulation started");
-        String serviceAccountKeyPath = "./g-code-editor-417ccbad5803.json";
-        GoogleCredentials credentials = ServiceAccountCredentials.fromStream(new FileInputStream(serviceAccountKeyPath))
-                .createScoped("https://www.googleapis.com/auth/cloud-platform");
-        AccessToken accessToken = credentials.refreshAccessToken();
-        String token = accessToken.getTokenValue();
-        System.out.println("Access Token: " + token);
-       uploadReport(token);
-        String reportName = "https://storage.googleapis.com/"+bucketName+"/" + App.reportName + ".html";
-        System.out.println("Report name: https://storage.googleapis.com/"+bucketName+"/" + App.reportName + ".html");
-        mongoTransfer(reportName);
-    }
-
-   public void uploadReport(String token) {
-       try {
-           System.out.println("in upload function");
-           String filePath = "./test-output/"+App.reportName+".html";
-           File file=new File(filePath);
-           RestAssured.baseURI = "https://storage.googleapis.com/upload/storage/v1/b/"+bucketName+"/o";
-           RestAssured.given().header("Authorization", "Bearer " + token).queryParam("uploadType","media").queryParam("name",App.reportName+".html").contentType("text/html").body(file).post().then().statusCode(200);
-       } catch (Exception e) {
-           throw new RuntimeException(e);
-       }
-   }
-
-    public void mongoTransfer(String reportName) throws IOException {
-        //uploading bucket report link and user-updated code to db
-        System.out.println("in mongo upload function");
-        String userId = App.reportName.split("_")[1];
-        // String url = "http://g-codeeditor.el.r.appspot.com/editor?name=" + userId;
-        String url=userId;
-        String filePath = "src/main/java/App.java";
-        String classContent = readClassFileAsString(filePath);
-        String escapedClassContent = classContent.replace("\"", "\\\"")
-                .replace("\n", "\\n")
-                .replace("\r", "\\r");
-        try {
-            URL getUrl = new URL("https://us-east-1.aws.data.mongodb-api.com/app/application-0-awqqz/endpoint/getSeleniumOutput"); // Replace with your actual GET API URL
-            HttpURLConnection getConnection = (HttpURLConnection) getUrl.openConnection();
-            getConnection.setRequestMethod("GET");
-
-            int getStatusCode = getConnection.getResponseCode();
-            if (getStatusCode == HttpURLConnection.HTTP_OK) {
-                try (BufferedReader reader = new BufferedReader(new InputStreamReader(getConnection.getInputStream()))) {
-                    StringBuilder response = new StringBuilder();
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        response.append(line);
-                    }
-
-                    String responseData = response.toString();
-                    JSONArray dataArray = new JSONArray(responseData); // Assuming the response is a JSON array
-
-                    String currentUrl = url; // Replace with the URL you want to compare
-
-                    boolean foundMatch = false;
-                    for (int i = 0; i < dataArray.length(); i++) {
-                        JSONObject item = dataArray.getJSONObject(i);
-                        String entryUrl = item.getString("url");
-
-                        if (entryUrl.equals(currentUrl)) {
-                            foundMatch = true;
-
-                            String apiUrl = "https://us-east-1.aws.data.mongodb-api.com/app/application-0-awqqz/endpoint/updateSeleniumSubmission";
-                            URL url1 = new URL(apiUrl);
-                            HttpURLConnection connection = (HttpURLConnection) url1.openConnection();
-                            connection.setRequestMethod("PUT");
-                            connection.setRequestProperty("Content-Type", "application/json");
-                            connection.setDoOutput(true);
-
-                            String putData = "{\n" +
-                                    "    \"filter\": {\n" +
-                                    "        \"url\": \"" + url + "\"\n" +
-                                    "    },\n" +
-                                    "    \"SubmittedCode\":\"" + escapedClassContent + "\",\n" +
-                                    "    \"Output\":\"" + reportName + "\"\n" +
-                                    "}";
-
-                            try (DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream())) {
-                                outputStream.writeBytes(putData);
-                                outputStream.flush();
-                            }
-                            int statusCode = connection.getResponseCode();
-                            String statusMessage = connection.getResponseMessage();
-                            System.out.println("Status Code: " + statusCode);
-                            System.out.println("Status Message: " + statusMessage);
-                            break; // No need to continue the loop once a match is found
-                        }
-                    }
-
-                    if (!foundMatch) {
-                        String apiUrl = "https://us-east-1.aws.data.mongodb-api.com/app/application-0-awqqz/endpoint/addSeleniumResult";
-                        URL url2 = new URL(apiUrl);
-                        HttpURLConnection connection = (HttpURLConnection) url2.openConnection();
-                        connection.setRequestMethod("PUT");
-                        connection.setRequestProperty("Content-Type", "application/json");
-                        connection.setDoOutput(true);
-                        String putData1 = "{\n" +
-                                "    \"filter\": {\n" +
-                                "        \"url\": \"" + url + "\"\n" +
-                                "    },\n" +
-                                "    \"code\":\"" + escapedClassContent + "\",\n" +
-                                "    \"output\":\"" + reportName + "\"\n" +
-                                "}";
+}
 
 
-                        try (DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream())) {
-                            outputStream.writeBytes(putData1);
-                            outputStream.flush();
-                        }
-                        int statusCode = connection.getResponseCode();
-                        String statusMessage = connection.getResponseMessage();
-                        System.out.println("Status Code: " + statusCode);
-                        System.out.println("Status Message: " + statusMessage);
-                    }
-                }
-            } else {
-                System.out.println("GET Request failed with status code " + getStatusCode);
-            }
 
-            getConnection.disconnect();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }

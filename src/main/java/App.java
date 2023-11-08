53976f1f99ee099d0aa0d63b3f86020d3573e16c
gem-rahulAdhikari <103276055+gem-rahulAdhikari@users.noreply.github.com>
2023-11-08 13:07:39 +0530

    Update YAML and Java files

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,57 +11,24 @@ import java.awt.*;
 import java.math.*;
 import java.time.*;
 
-public class App extends driverConfig{
-    static String reportName="Report_d57bc5785be7dfc774f8b69c8f08e3556c4d94060006fd5f91c51b0feb82f742_0";
+public class App extends driverConfig {
+    static String reportName="Report_1699429054_0";
 
-@Test
-public void demo(){
-driver.get("https://www.google.com/");
-     WebElement searchInput = driver.findElement(By.xpath("//textarea"));
-    searchInput.sendKeys("selenium");
-    searchInput.sendKeys(Keys.RETURN);
-     WebElement title = driver.findElement(By.xpath("(//h3[text()='Selenium'])[1]")); 
-     String fetchedTitle=title.getText();
-    System.out.println(fetchedTitle+" start2");
-     if("Selenium".equals(fetchedTitle))
-     {
-         extentTest.log(Status.PASS,"text matched successfully.",captureScreenshot());
-     }
-    else
-     {
-         extentTest.log(Status.FAIL,"Failed to match text.",captureScreenshot());
-     }
+    @Test
+    public void demo() {
+        driver.get("https://www.google.com/");
+        WebElement searchInput = driver.findElement(By.xpath("//textarea"));
+        searchInput.sendKeys("selenium");
+        searchInput.sendKeys(Keys.RETURN);
+        WebElement title = driver.findElement(By.xpath("(//h3[text()='Selenium'])[1]"));
+        String fetchedTitle = title.getText();
+        System.out.println(fetchedTitle + " start2");
+        if ("Selenium TEST".equals(fetchedTitle)) {
+            extentTest.log(Status.PASS, "text matched successfully.", captureScreenshot());
+        } else {
+            extentTest.log(Status.FAIL, "Failed to match text.", captureScreenshot());
+        }
 
-     extentTest.log(Status.PASS,driver.getCurrentUrl(),captureScreenshot());
-     extentTest.log(Status.PASS,driver.getCurrentUrl(),captureScreenshot());
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        extentTest.log(Status.PASS, driver.getCurrentUrl(), captureScreenshot());
+    }
 }
